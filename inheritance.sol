53071321e9800b85222b752137003d7cfdf118a3
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-05-09 21:30:24 +0300

    Create inheritance.sol

---
+++ inheritance.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity >=0.8.2 <0.9.0;
+
+contract Animal {
+    function live() external pure returns (bool) {
+        return true;
+    }
+}
+
+contract Dog is Animal {
+    function bark() external returns (uint256) {
+        return 1;
+    }
+}
