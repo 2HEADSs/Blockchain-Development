ae84cd79f55b633ea4c5e1b91e0f8dd67bfce6cc
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-05-09 19:00:04 +0300

    Update data-types.sol

--- data-types.sol
+++ data-types.sol
@@ -4,11 +4,18 @@ pragma solidity >=0.8.2 <0.9.0;
 
 contract Test {
     uint256[5] public arr = [1, 2, 3, 4, 5];
+    uint256 a;
 
     function sumNums() external view returns (uint256) {
         uint256 res;
         for (uint256 i = 0; i < arr.length; i++) {
             res += arr[i];
         }
+    }
+
+    function storageArray() external {
+        uint[5] storage numbers = arr;
+
+        numbers[1] = 2;
     }
 }
