02d21d77d400b7323b68e52f26451c8bea8b15a4
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-05-10 19:44:42 +0300

    Update Homeworks.sol

--- 04.Solidity in Debt/Homeworks.sol
+++ 04.Solidity in Debt/Homeworks.sol
@@ -9,13 +9,59 @@ pragma solidity >=0.8.2 <0.9.0;
  */
 
 contract DecentralizedAuctionPlatform {
-    struct AuctionData {
+    struct Auction {
+        uint256 auctionId;
+        string name;
+        string description;
+        uint256 startingPrice;
         uint256 startTime;
         uint256 duration;
-        string itemName;
-        string itemDescription;
-        uint256 startingPrice;
+        bool finishedAction;
+    }
+    Auction[] public Auctions;
+    uint256 public auctionCount = 0;
+
+    event createActionInfo(
+        uint256 startTime,
+        uint256 duration,
+        string name,
+        string description,
+        uint256 startingPrice
+    );
+
+    function createAuction(
+        uint256 startTime,
+        uint256 duration,
+        string memory name,
+        string memory description,
+        uint256 startingPrice
+    ) public {
+        require(
+            startTime > block.timestamp,
+            "The time must be bigger than timestam"
+        );
+        require(duration > 0, "Time must be pozitive");
+        auctionCount++;
+
+        Auctions.push(
+            Auction(
+                auctionCount,
+                name,
+                description,
+                startingPrice,
+                startTime,
+                duration,
+                false
+            )
+        );
+        emit createActionInfo(
+            startTime,
+            duration,
+            name,
+            description,
+            startingPrice
+        );
     }
 
-    function createAuction() public {}
+
 }
