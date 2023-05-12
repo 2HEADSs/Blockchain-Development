864f478e1604720330801861fbdb5c540e41cf56
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-05-12 21:01:56 +0300

    Create ERC-721.sol

---
+++ 04.Solidity in Debt/ERC-721.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity >=0.8.2 <0.9.0;
+
+/**
+ * @title Storage
+ * @dev Store & retrieve value in a variable
+ * @custom:dev-run-script ./scripts/deploy_with_ethers.ts
+ */
