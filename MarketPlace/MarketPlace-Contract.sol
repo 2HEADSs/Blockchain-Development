eb23e0604ffae3d11c44aa0b4d0e2796323b70bb
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-05-13 12:11:08 +0300

    Update MarketPlace-Contract.sol

--- MarketPlace/MarketPlace-Contract.sol
+++ MarketPlace/MarketPlace-Contract.sol
@@ -3,17 +3,23 @@ pragma solidity ^0.8.4;
 
 import "./Event-to-MarketPlace.sol";
 
-
 contract Marketplace {
-    address [] public  events;
+    address[] public events;
 
     event NewEvent(address creator, address eventAddr);
-    
-    function createEvent() external {
-        address newEvent = address (new Event());
+
+    function createEvent(
+        uint256 _saleStart,
+        uint256 _saleEnd,
+        uint256 _ticketPric,
+        string memory _metadata
+    ) external {
+        address newEvent = address(
+            new Event(_saleStart, _saleEnd, _name, _ticketPric, _metadata)
+        );
 
         events.push(newEvent);
 
-        emit NewEvent(msg.sender,newEvent);
+        emit NewEvent(msg.sender, newEvent);
     }
 }
