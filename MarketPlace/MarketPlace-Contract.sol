6dccac2e5a54afba83978326721d1ce4ba9b22a8
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-05-13 11:36:46 +0300

    Update MarketPlace-Contract.sol

--- MarketPlace/MarketPlace-Contract.sol
+++ MarketPlace/MarketPlace-Contract.sol
@@ -1,7 +1,19 @@
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
+import "./Event-to-MarketPlace.sol";
+
+
 contract Marketplace {
-    function createEvent(){
-        address newEvent = new Event()
+    address [] public  events;
+
+    event NewEvent(address creator, address eventAddr);
+    
+    function createEvent() external {
+        address newEvent = address (new Event());
+
+        events.push(newEvent);
+
+        emit NewEvent(msg.sender,newEvent);
     }
 }
