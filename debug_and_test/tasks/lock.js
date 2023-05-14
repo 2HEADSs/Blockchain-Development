91d7f4b1a334e2882b9e8f212153ffa9ce11398f
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-05-14 13:25:54 +0300

    Update lock.js

--- debug_and_test/tasks/lock.js
+++ debug_and_test/tasks/lock.js
@@ -1,11 +1,15 @@
 const { task } = require("hardhat/config");
 
 task("accounts", "Prints the list of accounts")
-    .addParam("accNum", "acount numbers")
+    .addParam("acc", "acount numbers")
     .setAction(async (taskArgs, hre) => {
         const accounts = await hre.ethers.getSigners();
-        for (const account of accounts) {
-            console.log(account.address);
+        // for (const account of accounts) {
+        //     console.log(account.address);
+        // }
+
+        for (let i = 0; i < taskArgs.acc; i++) {
+            console.log(accounts[i].address);
         }
     });
 
