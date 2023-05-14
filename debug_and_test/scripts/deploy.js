1b673cfe048b8b30b03bd95b362108da14f16d8b
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-05-14 13:33:20 +0300

    Update deploy.js

--- debug_and_test/scripts/deploy.js
+++ debug_and_test/scripts/deploy.js
@@ -4,7 +4,7 @@ const hre = require("hardhat");
 
 async function main() {
   const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+  const unlockTime = currentTimestampInSeconds + 1;
 
   const lockedAmount = hre.ethers.utils.parseEther("0.001");
 
@@ -20,9 +20,3 @@ async function main() {
   );
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
