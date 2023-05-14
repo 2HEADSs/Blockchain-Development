5f27c49f9c73db5e2740426a8b635faa3851b265
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-05-14 13:08:56 +0300

    Update hardhat.config.js

--- debug_and_test/hardhat.config.js
+++ debug_and_test/hardhat.config.js
@@ -3,5 +3,13 @@ require("@nomicfoundation/hardhat-toolbox");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+    },
+    sepolia: {
+      url: "https://sepolia.infura.io/v3/<key>",
+    },
+    accounts: [privateKey1, privateKey2],
+  },
   solidity: "0.8.18",
 };
