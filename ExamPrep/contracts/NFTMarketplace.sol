2b5b4bfd05c80fa23dc8e1e0e638e38e67f8338e
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-05-27 13:56:46 +0300

    Update NFTMarketplace.sol

--- ExamPrep/contracts/NFTMarketplace.sol
+++ ExamPrep/contracts/NFTMarketplace.sol
@@ -1,6 +1,19 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.17;
 
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+
+
 import "./NFT.sol";
 
-contract NFTMarketplace is NFT {}
+contract NFTMarketplace is NFT {
+    
+    mapping (address => mapping(uint256=> uint256)) public nftPrice;
+
+    function listNFTForSale(address collection, uint256 id, uint256 price) external{
+        require(price !=0, "price must be greater than 0"); 
+        nftPrice[collection][id] = price;
+
+        IERC721(collection).transferFrom(msg.sender, address(this), id)
+    }
+}
