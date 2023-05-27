687e0ba8f09e403425a46ba6353ac810dbc09f56
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-05-27 17:25:11 +0300

    Update NFTMarketplace.js

--- ExamPrep/test/NFTMarketplace.js
+++ ExamPrep/test/NFTMarketplace.js
@@ -6,9 +6,16 @@ const { anyValue } = require("@nomicfoundation/hardhat-chai-matchers/withArgs");
 const { expect } = require("chai");
 
 describe("NFTMarketplace", function () {
-  // We define a fixture to reuse the same setup in every test.
-  // We use loadFixture to run this setup once, snapshot that state,
-  // and reset Hardhat Network to that snapshot in every test.
+
+  let marketplaceFirstUser, deployer, firstUser;
+
+
+  this.beforeAll(async function () {
+    [deployer, firstUser] = await ethers.getSigners();
+    const { marketplace } = await loadFixture(deployAndMint);
+    marketplaceFirstUser = getFirstUserMarketPlace(marketplace, firstUser);
+  });
+
   async function deployAndMint() {
     // const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
     // const ONE_GWEI = 1_000_000_000;
@@ -17,26 +24,41 @@ describe("NFTMarketplace", function () {
     // const unlockTime = (await time.latest()) + ONE_YEAR_IN_SECS;
 
     // Contracts are deployed using the first signer/account by default
-    const [deployer, firstUser] = await ethers.getSigners();
 
     const MarketplaceFactory = await ethers.getContractFactory("NFTMarketplace", deployer);
 
-    const marketplace = await Marketplaceock.deploy();
+    const marketplace = await MarketplaceFactory.deploy();
 
-    const marketplaceFirstUser = marketplace.connect(firstUser);
+    const _marketplaceFirstUser = marketplace.connect(firstUser);
 
-    const tx = await marketplace.createNFT("ttest");
+    await _marketplaceFirstUser.createNFT("ttest");
 
     return { marketplace, deployer, firstUser };
   }
 
   describe("Listing", function () {
-    it("Revert when price == 0", async function () {
+    it("reverts when price == 0", async function () {
+      console.log('hi');
       const { marketplace, deployer } = await loadFixture(deployAndMint);
 
-      await expect(marketplace.listNFT(marketplace.address, 0, 0)).to.be.revertedWith("price must be greater than 0")
-
-      expect(await lock.unlockTime()).to.equal(unlockTime);
+      await expect(marketplace.listNFTForSale(marketplace.address, 0, 0))
+        .to.be.revertedWith("price must be greater than 0");
     });
-  })
+  });
+
+  it("reverts when already listed", async function () {
+    const { marketplace, deployer } = await loadFixture(deployAndMint);
+
+    const price = ethers.utils.parseEther("1");
+    await marketplaceFirstUser.approve(marketplace.address, 0)
+    await marketplaceFirstUser.listNFTForSale(marketplace.address, 0, price);
+
+    await expect(marketplaceFirstUser.listNFTForSale(marketplace.address, 0, price))
+      .to.be.revertedWith("NFT is already listed for sale");
+  });
 });
+
+
+function getFirstUserMarketPlace(marketplace, firstUser) {
+  return marketplace.connect(firstUser);
+};
