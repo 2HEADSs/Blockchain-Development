8e3209aeb5cc301e57fd777bd37b77124d7b8c28
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-05-27 11:09:30 +0300

    Update NFT.sol

--- ExamPrep/contracts/NFT.sol
+++ ExamPrep/contracts/NFT.sol
@@ -20,8 +20,6 @@ contract MyToken is ERC721, ERC721URIStorage, Ownable {
         _setTokenURI(tokenId, uri);
     }
 
-    // The following functions are overrides required by Solidity.
-
     function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
         super._burn(tokenId);
     }
