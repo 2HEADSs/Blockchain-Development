c42c2626f8928ad90ffab29b3b4d028092f08f93
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-05-28 09:30:51 +0300

    Update index.js

--- ExamPrep/tasks/index.js
+++ ExamPrep/tasks/index.js
@@ -1,3 +1,14 @@
-task("balance", "Prints an account's balance")
-  .addParam("account", "The account's address")
-  .setAction(async () => {});
+task("deploy", "Prints an account's balance")
+    .addParam("account", "The account's address")
+    .setAction(async () => {
+        const Lock = await hre.ethers.getContractFactory("Lock");
+        const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+
+        await lock.deployed();
+
+        console.log(
+            `Lock with ${ethers.utils.formatEther(
+                lockedAmount
+            )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
+        );
+    });
