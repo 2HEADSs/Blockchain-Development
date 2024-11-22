2def347ac9c23f70202a9d3beb92c6aef93b39bf
2HEADSs <2heads.pd@gmail.com>
2024-11-22 18:19:59 +0200

    implement TempConversation contract

--- TemperatureConversion.sol
+++ TemperatureConversion.sol
@@ -1,5 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.28;
 
+contract TempConversation {
+    function toFahrenheit(uint256 celsius) public pure returns (uint256) {
+        return celsius + 32;
+    }
 
-
+    function toCelsius(uint256 fahrenheit) public pure returns (uint256) {
+        return fahrenheit - 32;
+    }
+}
