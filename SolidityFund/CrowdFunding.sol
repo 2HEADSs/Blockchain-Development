6eebe2ebd70f7e272b5fd099052c89c216687584
2HEADSs <2heads.pd@gmail.com>
2024-11-28 21:56:58 +0200

    create fixed array in test contract

--- SolidityFund/CrowdFunding.sol
+++ SolidityFund/CrowdFunding.sol
@@ -8,3 +8,16 @@ contract CrowdFunding {
         shares[receiver] += 1000;
     }
 }
+
+contract testFixedArray {
+    uint256[5] public arr = [1, 2, 3, 4, 5];
+
+    function addNumber() external view returns (uint256) {
+        uint256 res;
+        for (uint256 i = 0; i < arr.length; i++) {
+            res += arr[i];
+        }
+        return res;
+    }
+}
+
