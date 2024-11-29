9ee071b075118c72777652cb9a7f75399fac3e83
2HEADSs <2heads.pd@gmail.com>
2024-11-29 17:46:52 +0200

    create MessageBoard contract

---
+++ SolidityFund/MessageBoard.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.28;
+
+contract MessageBoard {
+    mapping(address => string[]) public messages;
+
+    function keepMessages(string calldata message) external {
+        messages[msg.sender].push(message);
+    }
+    function previewMessage(string calldata message) external pure returns (string memory) {
+        return string(abi.encode("Draft: ", message));
+    }
+
+}
