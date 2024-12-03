cbc671c859e1a88f7b92f57d3999a654db27ebaf
2HEADSs <2heads.pd@gmail.com>
2024-12-03 10:47:14 +0200

    Implement SavingAccounts

--- SolidityFund/DecentralizedSavingsAccount.sol
+++ SolidityFund/DecentralizedSavingsAccount.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.28;
 
-contract SavingAccounts{
+contract SavingAccounts {
     struct SavingAccount {
         uint256 balance;
         address owner;
@@ -11,8 +11,24 @@ contract SavingAccounts{
 
     mapping(address => SavingAccount[]) savings;
 
-    function createSavingsPlan(uint256 periodLimit) public payable {
-        require(msg.value >0, "Must send some Ether to create a saving account");
+    modifier validIndex(uint256 index) {
+        require(
+            index >= 0 && index < savings[msg.sender].length,
+            "Invalid Index"
+        );
+        _;
+    }
+
+    function seeAccounts() public view returns (uint256 length) {
+        return savings[msg.sender].length;
+    }
+
+    function createSavingPlan(uint256 periodLimit) public payable {
+        require(
+            msg.value > 0,
+            "Must send some Ether to create a saving account"
+        );
+
         savings[msg.sender].push(
             SavingAccount({
                 balance: msg.value,
@@ -21,5 +37,25 @@ contract SavingAccounts{
                 lockPeriod: periodLimit
             })
         );
+    }
+
+    function viewSavingsPlan(uint256 index)
+        public
+        view
+        validIndex(index)
+        returns (uint256 balance)
+    {
+        return (savings[msg.sender][index].balance);
+    }
+
+    function withdrawFunds(uint256 index) public validIndex(index) {
+        SavingAccount storage saving = savings[msg.sender][index];
+        uint256 timeForLockong = saving.creationTime + saving.lockPeriod;
+
+        require(timeForLockong < block.timestamp, "Saving is stil locked");
+
+        uint256 amount = saving.balance;
+        saving.balance = 0;
+        payable(msg.sender).transfer(amount);
     }
 }
