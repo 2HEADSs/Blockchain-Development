7b7ebec5aeadda8b9c98323a4266538098e44e09
2HEADSs <2heads.pd@gmail.com>
2025-01-15 17:31:46 +0200

    update demo

--- Inheritance and Modularity/ExternalCall.sol
+++ Inheritance and Modularity/ExternalCall.sol
@@ -2,19 +2,40 @@
 pragma solidity 0.8.28;
 
 contract InfoFeed {
+    bool public passed = false;
+
     function info() external payable returns (uint256 result) {
+        revert();
         result = 42;
+    }
+
+    receive() external payable {}
+
+    fallback() external {
+        passed = true;
     }
 }
 
-contract Consumer {
-    InfoFeed feed;
+interface IMyInfoFeed {
+    function infoTwo() external payable returns (uint256 result);
+}
 
-    function setFeed(InfoFeed addr) public {
+contract Consumer {
+    IMyInfoFeed feed;
+
+    function setFeed(IMyInfoFeed addr) public {
         feed = addr;
     }
 
-    function callFeed() public payable returns (uint256 feedResult) {
-        feedResult = feed.info{value: 10, gas: 800}();
+    // function callFeed() public payable returns (uint256 feedResult) {
+    //     feedResult = feed.info{value: 10, gas: 800}();
+    // }
+
+    function sendViaSend(address payable _to) public payable {
+        _to.transfer(msg.value);
+    }
+
+    function callFeed() public payable {
+        feed.infoTwo{value: msg.value}();
     }
 }
