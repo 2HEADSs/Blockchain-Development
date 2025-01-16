7e3d9756aef0371c4d6cda8d9cb2cb8bf948ef51
2HEADSs <2heads.pd@gmail.com>
2025-01-17 00:34:49 +0200

    update ERC20

--- SmartContractStandarts/ERC20.sol
+++ SmartContractStandarts/ERC20.sol
@@ -7,20 +7,24 @@ error InsufficientApproval();
 contract ERC20 {
     string public name;
     string public symbol;
-    uint8 public decimals = 3; // 1000 => 1 token; 10***
+    uint8 public decimals = 3; // 1000 => 1 token; 10**decimels
     uint256 public totalSupply;
-    mapping(address => uint256) balanceoF;
+    mapping(address => uint256) public balanceoF;
 
-    mapping(address => mapping(address => uint256)) public allowance;
+    mapping(address => mapping(address => uint256)) public approvals;
 
     constructor(string memory newName, string memory newSymbol) {
         name = newName;
         symbol = newSymbol;
+
+        uint256 supply = 1000 * 10**decimals;
+        balanceoF[msg.sender] = supply;
+        totalSupply = supply;
     }
 
     event Transfer();
 
-    function transferFrom(address _to, uint256 _value)
+    function transfer(address _to, uint256 _value)
         public
         returns (bool success)
     {
@@ -35,11 +39,34 @@ contract ERC20 {
         return true;
     }
 
+    function transferFrom(
+        address _from,
+        address _to,
+        uint256 _value
+    ) public returns (bool success) {
+        if (approvals[_from][msg.sender] < _value) {
+            revert InsufficientApproval();
+        }
+
+        approvals[_from][msg.sender] -= _value;
+        
+        if (balanceoF[_from] < _value) {
+            revert InsufficientBalance();
+        }
+
+        balanceoF[_from] -= _value;
+        balanceoF[_to] += _value;
+
+        emit Transfer();
+
+        return true;
+    }
+
     function approve(address _spender, uint256 _value)
         public
         returns (bool success)
     {
-        allowance[msg.sender][_spender] = _value;
+        approvals[msg.sender][_spender] = _value;
         return true;
     }
 }
