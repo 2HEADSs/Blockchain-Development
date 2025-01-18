53023f39d7430b4e3498e2ed0e18509a1f523a39
2HEADSs <2heads.pd@gmail.com>
2025-01-18 23:34:39 +0200

    implement ParityChecker contract with library

--- Libraries and Tools/MathLib.sol
+++ Libraries and Tools/MathLib.sol
@@ -1,8 +1,16 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.28;
 
-library MathLib {
-    function add(uint256 a, uint256 b) internal pure returns (uint256 result) {
-        result = a + b;
+library NumLib {
+    function isEven(uint256 self) internal pure returns (bool) {
+        return self % 2 == 0;
+    }
+}
+
+contract ParityChecker {
+    using NumLib for uint256;
+
+    function checkParity(uint256 number) external pure returns (bool) {
+        return number.isEven();
     }
 }
