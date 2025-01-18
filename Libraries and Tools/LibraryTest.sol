730aef19add70a75a4a5c4b2f17f10d34d314459
2HEADSs <2heads.pd@gmail.com>
2025-01-18 22:48:20 +0200

    using "method"

--- Libraries and Tools/LibraryTest.sol
+++ Libraries and Tools/LibraryTest.sol
@@ -4,7 +4,8 @@ pragma solidity 0.8.28;
 import {MathLib} from "./MathLib.sol";
 
 contract Calculator {
+    using MathLib for uint256;
     function addDirectCall(uint256 a, uint256 b) public pure returns (uint256) {
-        return MathLib.add(a, b);
+        return a.add(b);
     }
 }
