150e0b86c5143a138ec4cebcfcbac13a69a333af
2HEADSs <2heads.pd@gmail.com>
2025-01-22 19:36:31 +0200

    add update fn to event

--- Exercise/Event.sol
+++ Exercise/Event.sol
@@ -11,17 +11,22 @@ contract Event is ERC721, Ownable {
 
     // date is timestamp
     uint256 public immutable date;
+    address public immutable organizer;
+
     string public location;
     uint256 public ticketAvailability;
 
     constructor(
         address minter,
         string memory eventName,
+        uint256 date_,
         string memory location_,
-        uint256 date_
+        address organizer_
     ) ERC721(eventName, "MTK") Ownable(minter) {
         date = date_;
         location = location_;
+        organizer = organizer_;
+        _nextTokenId = 1;
     }
 
     function safeMint(address to) public onlyOwner {
@@ -30,5 +35,40 @@ contract Event is ERC721, Ownable {
         }
         uint256 tokenId = _nextTokenId++;
         _safeMint(to, tokenId);
+    }
+
+    function _update(
+        address to,
+        uint256 tokenId,
+        address auth
+    ) internal override returns (address) {
+        address from = _ownerOf(tokenId);
+
+        // Perform (optional) operator check
+        if (auth != address(0)) {
+            _checkAuthorized(from, auth, tokenId);
+        }
+
+        // Execute the update
+        if (from != address(0)) {
+            // Clear approval. No need to re-authorize or emit the Approval event
+            _approve(address(0), tokenId, address(0), false);
+
+            unchecked {
+                _balances[from] -= 1;
+            }
+        }
+
+        if (to != address(0)) {
+            unchecked {
+                _balances[to] += 1;
+            }
+        }
+
+        _owners[tokenId] = to;
+
+        emit Transfer(from, to, tokenId);
+
+        return from;
     }
 }
