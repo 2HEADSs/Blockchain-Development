8a5dc36ace836b9d263275cc37636b5aca783583
Pavel.Dimitrov <2heads.pd@gmail.com>
2025-01-24 16:30:09 +0200

    rearrange folders

--- Libraries and Tools/ParityChecker.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity 0.8.28;
-
-library MathLib {
-    function add(uint256 a, uint256 b) internal pure returns (uint256 result) {
-        result = a + b;
-    }
-}
