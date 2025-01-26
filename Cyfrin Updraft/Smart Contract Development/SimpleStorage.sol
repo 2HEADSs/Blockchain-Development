3a5cae5e34ea50534754bd227074f1ae6514a735
2HEADSs <2heads.pd@gmail.com>
2025-01-26 22:30:38 +0200

    add dynamic array

--- Cyfrin Updraft/Smart Contract Development/SimpleStorage.sol
+++ Cyfrin Updraft/Smart Contract Development/SimpleStorage.sol
@@ -2,9 +2,31 @@
 pragma solidity 0.8.28;
 
 contract SimpleStorage {
-    uint256 public favoriteNumber;
+    uint256 myFavoriteNumber;
+
+    // uint256[] favoriteNumbers;
+    struct Person {
+        uint256 favoriteNumber;
+        string name;
+    }
+
+    Person[] public listOfPeople;
+
+    // Person public pat = Person({favoriteNumber: 7, name: "Pat"});
+    // Person public mariah = Person({favoriteNumber: 16, name: "Mariah"});
+    // Person public jon = Person({favoriteNumber: 12, name: "Jon"});
+
+    // Person public pat = Person(7, "Pat");
 
     function store(uint256 _favoriteNumber) public {
-        favoriteNumber = _favoriteNumber;
+        myFavoriteNumber = _favoriteNumber;
+    }
+
+    function retrive() public view returns (uint256) {
+        return myFavoriteNumber;
+    }
+
+    function addPerson(string memory name, uint256 _favouriteNumber) public {
+        listOfPeople.push(Person(_favouriteNumber, name));
     }
 }
