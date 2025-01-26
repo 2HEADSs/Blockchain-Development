8200eb038e3409ef91189b92eacabbbef25ec0f6
Pavel.Dimitrov <2heads.pd@gmail.com>
2025-01-26 19:59:18 +0200

    implement Send ETH to given address

--- Ethereum-SoftUni/Hardhat/tasks/index.js
+++ Ethereum-SoftUni/Hardhat/tasks/index.js
@@ -9,10 +9,11 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 
 task("balance", "Prints an account's balance")
     .addParam("address", "the addres to send to")
-    .setAction(async (_, hre) => {
+    .setAction(async (taskArgs, hre) => {
         // const provider = new ethers.JsonRpcProvider("http://127.0.0.1:8545");
         const provider = hre.ethers.provider;
-        const res = await provider.getBalance("0x70997970C51812dc3A010C7d01b50e0d17dc79C8");
+        // const res = await provider.getBalance("0x70997970C51812dc3A010C7d01b50e0d17dc79C8");
+        const res = await provider.getBalance(taskArgs.address);
         console.log(res);
         console.log("result in ETH", ethers.formatEther(res));
     });
@@ -33,6 +34,19 @@ task("send", "Send ETH to given address")
         const receipt = await tx.wait();
         console.log("Tx mined");
         console.log(receipt);
+    });
+
+
+task("contract", "Send ETH to given address")
+    .setAction(async (taskParams, hre) => {
+        const ContractFactory = await hre.ethers.getContractFactory("Lock");
+        const contract = await ContractFactory.deploy(1737986281);
+        console.log("Contract deployed to:", await contract.getAddress());
+
+        const tx = contract.withdraw().catch(
+            err => console.log(err.message));
+        console.log("Tx send!");
+
     });
 
 
