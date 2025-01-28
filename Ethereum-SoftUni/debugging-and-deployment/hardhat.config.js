59210e05bf11356679fb7284873576f19553c403
Pavel.Dimitrov <2heads.pd@gmail.com>
2025-01-28 21:38:24 +0200

    set .env and alchemy configurations

--- Ethereum-SoftUni/debugging-and-deployment/hardhat.config.js
+++ Ethereum-SoftUni/debugging-and-deployment/hardhat.config.js
@@ -1,7 +1,18 @@
+const env = require("hardhat");
+
 require("@nomicfoundation/hardhat-toolbox");
 require("./tasks/index.js");
+
+require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.28",
+  networks: {
+    sepolia: {
+      url: process.env.WEB3_PROIDER_URL,
+      accounts: [process.env.PRIVATE_KEY]
+    }
+  }
+
 };
--- Ethereum-SoftUni/debugging-and-deployment/tasks/index.js
+++ Ethereum-SoftUni/debugging-and-deployment/tasks/index.js
@@ -1,7 +1,10 @@
-task("deploy", "Deploy contract", async(_,hre)=>{
+task("deploy", "Deploy contract", async (_, hre) => {
+    if (!taskArgs.unlockTime) {
+        throw new Error("unlocTtime is required")
+    }
+
     const ContractFactory = await hre.ethers.getContractFactory("Lock");
     const contract = await ContractFactory.deploy(1738079698);
 
     console.log("Contract deployed to:", contract.target);
-    
-})
+}).addParam("unlockTime", "The unix timestamp after which the contract will be unlocked.")
