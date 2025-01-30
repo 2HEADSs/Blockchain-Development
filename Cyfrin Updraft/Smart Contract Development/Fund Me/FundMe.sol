d4e1ae2936447c03fef9ec6bdb52a9d06384373b
2HEADSs <2heads.pd@gmail.com>
2025-01-30 12:45:10 +0200

    add require to fund Fn

--- Cyfrin Updraft/Smart Contract Development/Fund Me/FundMe.sol
+++ Cyfrin Updraft/Smart Contract Development/Fund Me/FundMe.sol
@@ -2,7 +2,9 @@
 pragma solidity 0.8.28;
 
 contract FundMe {
-    function fund() public {}
+    function fund() public payable {
+        require(msg.value > 1e18, "didn't send enough ETH");
+    }
 
-    function withdraw() public {}
+    // function withdraw() public {}
 }
