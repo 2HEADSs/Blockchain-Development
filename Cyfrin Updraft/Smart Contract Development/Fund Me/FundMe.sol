fccc98bd45c7a2f2f9d6bb7a95e96404d69f9215
Pavel.Dimitrov <2heads.pd@gmail.com>
2025-01-31 22:33:11 +0200

    update FundMe

--- Cyfrin Updraft/Smart Contract Development/Fund Me/FundMe.sol
+++ Cyfrin Updraft/Smart Contract Development/Fund Me/FundMe.sol
@@ -1,22 +1,39 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.28;
-// import {AggregatorV3Interface} from "contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
 import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
 
 contract FundMe {
     uint256 public minimumUsd = 5;
 
     function fund() public payable {
-        require(msg.value > minimumUsd, "didn't send enough ETH");
+        require(
+            getConversionRate(msg.value) >= minimumUsd,
+            "didn't send enough ETH"
+        );
     }
 
     // function withdraw() public {}
 
-    function getPrice() public {
+    function getPrice() public view returns (uint256) {
         // 0x694AA1769357215DE4FAC081bf1f309aDC325306
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(
+            0x1b44F3514812d835EB1BDB0acB33d3fA3351Ee43
+        );
+        (, int256 price, , , ) = priceFeed.latestRoundData();
+        //Price of ETH in terms of USD
+
+        return uint256(price * 1e10);
     }
 
-    function getConversionRate() public {}
+    function getConversionRate(uint256 ethAmount)
+        public
+        view
+        returns (uint256)
+    {
+        uint256 ethPrice = getPrice();
+        uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1e18;
+        return ethAmountInUsd;
+    }
 
     function getVersion() public view returns (uint256) {
         return
