508e20d7d7f29f69738d3934322019cdfefc53bb
Pavel.Dimitrov <2heads.pd@gmail.com>
2025-02-15 16:56:48 +0200

    create SafeMatTester

---
+++ Cyfrin Updraft/Smart Contract Development/Fund Me/artifacts/SafeMathTester.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
+
+contract SafeMathTester {
+    uint8 public bigNumber = 255;
+
+    function add() public {
+        unchecked {
+            bigNumber = bigNumber + 1;
+        }
+    }
+}
