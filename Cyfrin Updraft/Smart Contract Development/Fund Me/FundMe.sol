f249562d170e4a0df355774e649003a2e959163a
Pavel.Dimitrov <2heads.pd@gmail.com>
2025-02-15 18:20:50 +0200

    implement withdraw

--- Cyfrin Updraft/Smart Contract Development/Fund Me/FundMe.sol
+++ Cyfrin Updraft/Smart Contract Development/Fund Me/FundMe.sol
@@ -7,6 +7,7 @@ contract FundMe {
     using PriceConverter for uint256;
 
     uint256 public minimumUsd = 5e18;
+
     address[] public funders;
     mapping(address => uint256) public addressToAmountFunded;
 
@@ -16,10 +17,29 @@ contract FundMe {
             "didn't send enough ETH"
         );
         funders.push(msg.sender);
-        addressToAmountFunded[msg.sender] =
-            addressToAmountFunded[msg.sender] +
-            msg.value;
+        addressToAmountFunded[msg.sender] += msg.value;
     }
 
-    // function withdraw() public {}
+    function withdraw() public {
+        for (
+            uint256 funderIndex = 0;
+            funderIndex < funders.length;
+            funderIndex++
+        ) {
+            address funder = funders[funderIndex];
+            addressToAmountFunded[funder] = 0;
+        }
+        funders = new address[](0);
+
+        // //transfer
+        // payable(msg.sender).transfer(address(this).balance);
+        // //send
+        // bool sendSuccess = payable(msg.sender).send(address(this).balance);
+        // require(sendSuccess, "Send failed");
+        // //call
+        (bool callSuccess, ) = payable(msg.sender).call{
+            value: address(this).balance
+        }("");
+        require(callSuccess, "Call failed");
+    }
 }
