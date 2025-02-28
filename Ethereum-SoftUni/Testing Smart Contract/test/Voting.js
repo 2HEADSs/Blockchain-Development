4722f1c9faee31a13dd83f8910e5428311354d0f
Pavel.Dimitrov <2heads.pd@gmail.com>
2025-02-28 16:57:15 +0200

    add test for Voting contract

--- Ethereum-SoftUni/Testing Smart Contract/test/Voting.js
+++ Ethereum-SoftUni/Testing Smart Contract/test/Voting.js
@@ -1,4 +1,4 @@
-const { loadFixture } = require("@nomicfoundation/hardhat-toolbox/network-helpers")
+const { loadFixture, } = require("@nomicfoundation/hardhat-toolbox/network-helpers")
 const { expect } = require("chai");
 
 describe.only("Voting", function () {
@@ -14,8 +14,29 @@ describe.only("Voting", function () {
     describe("Deployment", async function () {
         it("Should set the right owner", async function () {
             const { VotingContract, deployer } = await loadFixture(deployVotingFixture);
+            expect(await VotingContract.owner()).to.be.equal(deployer.address);
         });
 
-        expect(await VotingContract.owner()).to.be.equal(deployer.addres);
+        it("Should set proposal count to 0", async function () {
+            const { VotingContract, deployer } = await loadFixture(deployVotingFixture);
+            expect(await VotingContract.proposalCount()).to.be.equal(0);
+        });
     });
+
+    describe("createProposal()", function () {
+        it("Should revert when caller is not owner", async function () {
+            const { VotingContract } = await loadFixture(deployVotingFixture);
+            const [, user] = await ethers.getSigners();
+            await expect(VotingContract.connect(user).createProposal("testDescription", 1)
+            ).to.be.revertedWithCustomError(VotingContract, "NotOwner");
+        });
+
+
+        it("Should revert when voting period is zero", async function () {
+            const { VotingContract } = await loadFixture(deployVotingFixture);
+            await expect(VotingContract.createProposal("testDescription", 0)
+            ).to.be.revertedWithCustomError(VotingContract, "InvalidVotingPeriod");
+        });
+    })
+
 });
