75035f6fade4fe2aadd81b75b100110db5a0f764
Pavel.Dimitrov <2heads.pd@gmail.com>
2025-03-10 13:42:49 +0200

    implement custom NFT

---
+++ Blockchain-SoftUni-Fund/ExamPrep/contracts/NFT.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.25;
+
+import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+
+contract MyToken is ERC721, Ownable {
+    uint256 private _nextTokenId;
+
+    constructor(
+        address initialOwner
+    ) ERC721("MyToken", "MTK") Ownable(initialOwner) {
+        _customMint(msg.sender);
+    }
+
+    function safeMint(address to) public onlyOwner {
+        _customMint(to);
+    }
+
+    function _customMint(address to) internal {
+        uint256 tokenId = _nextTokenId++;
+        _safeMint(to, tokenId);
+    }
+}
