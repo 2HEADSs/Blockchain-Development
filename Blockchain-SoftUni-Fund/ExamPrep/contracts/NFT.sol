e1583c2fb2858bbfee453fb51932ea5a0214b01f
Pavel.Dimitrov <2heads.pd@gmail.com>
2025-03-11 11:21:46 +0200

    fix contract name

--- Blockchain-SoftUni-Fund/ExamPrep/contracts/NFT.sol
+++ Blockchain-SoftUni-Fund/ExamPrep/contracts/NFT.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.25;
 import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
-contract MyToken is ERC721, Ownable {
+contract NFT is ERC721, Ownable {
     uint256 private _nextTokenId;
 
     constructor(
